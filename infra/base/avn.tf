dd5de4b0690ad5fe56deb599df5dd5a0d4aaca01
chetanagra <jbpchetan@gmail.com>
2026-07-23 22:20:04 +0530

    Update avn.tf

--- infra/base/avn.tf
+++ infra/base/avn.tf
@@ -153,7 +153,7 @@ resource "azurerm_network_security_rule" "rule-inbound-ssh-backend" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "*"
+  source_address_prefix       = "VirtualNetwork"
   destination_address_prefix  = "VirtualNetwork"
   resource_group_name         = data.azurerm_resource_group.azure-resource.name
   network_security_group_name = azurerm_network_security_group.security-group-backend.name
