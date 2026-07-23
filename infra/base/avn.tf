48b1c7cf776c83092445662d9a3f8fb923ec0ecb
chetanagra <jbpchetan@gmail.com>
2026-07-23 21:59:55 +0530

    Update avn.tf

--- infra/base/avn.tf
+++ infra/base/avn.tf
@@ -226,7 +226,7 @@ resource "azurerm_network_interface" "network-interface-frontend" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.public_subnet_b.id
+    subnet_id                     = azurerm_subnet.public_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public-ip-frontend.id
   }
