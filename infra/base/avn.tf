466830045b4715064dd85a84af9a0f1076352cdb
chetanagra <jbpchetan@gmail.com>
2026-07-24 13:40:24 +0530

    Update avn.tf

--- infra/base/avn.tf
+++ infra/base/avn.tf
@@ -285,6 +285,7 @@ resource "azurerm_subnet_network_security_group_association" "association-ni-sg-
 resource "azurerm_network_interface_security_group_association" "association-ni-sg-frontend" {
   network_interface_id      = azurerm_network_interface.network-interface-frontend.id
   network_security_group_id = azurerm_network_security_group.security-group-frontend.id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lb-backend-pool.id
 }
 
 output "frontend_url" {
